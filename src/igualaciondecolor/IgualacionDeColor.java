a66965a0abb25e072f91f80d8dc672079489e7f0
alberto__segura <alberto.segura.delgado@gmail.com>
2016-03-03 13:59:47 +0100

    initial commit

---
+++ src/igualaciondecolor/IgualacionDeColor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package igualaciondecolor;
+
+/**
+ *
+ * @author alberto
+ */
+public class IgualacionDeColor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GUI miGui = new GUI();
+        miGui.setVisible(true);
+    }
+    
+}
